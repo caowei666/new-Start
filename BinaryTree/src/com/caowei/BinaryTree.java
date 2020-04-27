7667373adfee9a09339c13cc520e9d53e55090ec
caowei666 <43978501+caowei666@users.noreply.github.com>
2020-04-27 08:02:33 +0800

    Add files via upload

---
+++ BinaryTree/src/com/caowei/BinaryTree.java
@@ -0,0 +1,48 @@
+package com.caowei;
+
+public class BinaryTree {
+	private Node root;
+	public void add(int data) {
+		if(root == null) {
+			root = new Node(data);
+		}else {
+			root.addNode(data);
+		}
+	}
+	public void print() {
+		root.printNode();
+	}
+	private class Node{
+		private int data;
+		private Node left;
+		private Node right;
+		public Node(int data) {
+			this.data = data;
+		}
+		public void addNode(int data) {
+			if(this.data>data) {
+				if(this.left == null) {
+					this.left = new Node(data);
+				}else {
+					this.left.addNode(data);
+				}
+			}else {
+				if(this.right == null) {
+					this.right = new Node(data);
+				}else {
+					this.right.addNode(data);
+				}
+			}
+		}
+		//ÖÐÐò±éÀú(×óÖÐÓÒ)		
+		public void printNode() {
+			if(this.left != null) {
+				this.left.printNode();
+			}
+			System.out.print(this.data+"->");
+			if(this.right!=null) {
+				this.right.printNode();
+			}
+		}
+	}
+}
---
+++ BinaryTree/src/com/caowei/BinaryTreeDemo.java
@@ -0,0 +1,21 @@
+package com.caowei;
+
+public class BinaryTreeDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BinaryTree bt = new BinaryTree();
+		bt.add(8);
+		bt.add(3);
+		bt.add(10);
+		bt.add(1);
+		bt.add(6);
+		bt.add(14);
+		bt.add(4);
+		bt.add(7);
+		bt.add(13);
+		
+		bt.print();
+	}
+
+}
